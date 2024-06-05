5af4cdb07089127b93d10b230180683fff9b6d11
JasserChaaben <chaabnjasser@gmail.com>
2024-06-05 12:09:05 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -21,15 +21,31 @@ class Background extends React.Component {
     this.state = {
 
     };
-  
+    this.playSound=this.playSound.bind(this);
   }
+  playSound(event){
+    document.querySelectorAll('audio.backgroundSound').forEach(Element=>Element.pause())
+    const audio = event.currentTarget.querySelector('audio');
+    if (audio) {
+      if (audio.paused) {
+        audio.play();
+    }else{
+        audio.currentTime = 5
+    }
+  }
+  const link="./image/"+audio.id+".jpg"
+  document.body.style.backgroundImage = 'url('+link+')';
+  document.body.style.backgroundSize = 'cover'; 
+}
   render() {
   
     return (
       <div>
-          <button><img src="./icons/school.png" alt="school Icon"></img></button>
-          <button><img src="./icons/tent.png" alt="tent Icon"></img></button>
-          <button><img src="./icons/traffic.png" alt="traffic Icon"></img></button>
+        <div id="buttons">
+          <div className='icon' onClick={this.playSound}><img src="./icons/school.png" alt="school Icon"></img><audio id="school" className="backgroundSound" loop src="./audio/School.mp3"/></div>
+          <div className='icon' onClick={this.playSound}><img src="./icons/tent.png" alt="tent Icon"></img><audio id="carnival" className="backgroundSound" loop src="./audio/Carnival.mp3"/></div>
+          <div className='icon' onClick={this.playSound}><img src="./icons/traffic.png" alt="traffic Icon"></img><audio id="traffic"  className="backgroundSound" loop src="./audio/City.mp3"/></div>
+          </div>
       </div>
     );
   }
